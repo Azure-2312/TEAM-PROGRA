6d33901eb982c2bea722bc1ec9f6c3b877aa272d
PIERO MONTALVO <pieromontalvo@gmail.com>
2026-05-21 14:10:25 -0500

    Corrige frontend como carpeta normal

---
+++ frontend/frontend-app/src/app/pages/history/history.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-history',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './history.html',
+  styleUrl: './history.scss'
+})
+export class History {}
